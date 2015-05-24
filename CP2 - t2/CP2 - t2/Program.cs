8323a69ddaa06fa44d7e6addb2fa94c7f1c82e1d
fylly <andrew@iter.by>
2015-05-25 00:33:29 +0300

    CP2 - t2
    ver 2

--- CP2 - t2/CP2 - t2/Program.cs
+++ CP2 - t2/CP2 - t2/Program.cs
@@ -9,43 +9,43 @@ namespace CP2___t1
     class Program
     {
         private static char [] _seperator = { ' ', '.','?',','};
-        private static String _pathIn = @"C:\MyFiles.txt";
-        private static String _pathOut = @"D:\MyFiles.txt";
+        private static String _pathIn = @"D:\MyFilesIn.txt";
+        private static String _pathOut = @"D:\MyFilesOut.txt";
 
         static void Main(string[] args)
         {
             WordsCollection wordsCollection = new WordsCollection();
-
-            // Read from file
-            //System.IO.StreamReader file = new System.IO.StreamReader(_path);
-           
+             
             try
             {
-                using (StreamReader sr = new StreamReader(_pathIn))
+                StreamReader sr=new StreamReader(_pathIn);
+                while (!sr.EndOfStream)
                 {
-                    while (sr.Peek() >= 0)
-                    {
-                        wordsCollection.Add(new WordsLine(sr.ReadLine(),_seperator));
-                    }
+                    wordsCollection.Add(new WordsLine(sr.ReadLine(),_seperator));
                 }
+                sr.Close();
             }
             catch (Exception e)
             {
-                
                 Console.WriteLine("The process failed: {0}", e.ToString());
             }
-           // var hg = new WordsLine(new String [] {"jhgh","a","a","j"});
-               
 
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(_pathOut))
+            
+            try
             {
+                StreamWriter sw = new StreamWriter(_pathOut);
                 foreach (var i in wordsCollection.GetSingleOrderWords())
                 {
-                    file.WriteLine("{0} - {1} - {2}",
+                    sw.WriteLine("{0} - {1} - {2}",
                     i,
                     wordsCollection.GetWordsLinesCount(i),
                     String.Join(",", wordsCollection.GetPositionsWord(i).ToArray()));                   
                 }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
             }
 
             Console.ReadKey();
